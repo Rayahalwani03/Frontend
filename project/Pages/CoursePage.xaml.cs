be3c1b4dd3b8f49020cac6b554fe37d7142ae438
Raya HALWANI <raya.halwani@st.uskudar.edu.tr>
2024-05-19 10:43:25 +0300

    Add project files.

---
+++ project/Pages/CoursePage.xaml.cs
@@ -0,0 +1,19 @@
+namespace project;
+
+public partial class CoursePage : ContentPage
+{
+	public CoursePage()
+	{
+		InitializeComponent();
+	}
+
+    private void Add_Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Show_Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ project/Pages/StudentPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace project;
+
+public partial class StudentPage : ContentPage
+{
+	public StudentPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Add_Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Show_Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
